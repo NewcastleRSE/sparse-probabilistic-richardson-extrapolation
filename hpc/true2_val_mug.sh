a91c5f307a802e7348a7d334a92201198dfce717
Richard Howey <richard.howey@ncl.ac.uk>
2025-12-04 10:02:31 +0000

    mug logs

--- hpc/true2_val_mug.sh
+++ hpc/true2_val_mug.sh
@@ -3,7 +3,7 @@
 #SBATCH --account=comet_spread
 #SBATCH --mem=2GB
 #SBATCH --cpus-per-task=1
-#SBATCH --output=data/physics_mug/results/output_physics_mug_true4_val.log
+#SBATCH --output=data/physics_mug/results/output_physics_mug_true5_val.log
 
 # Load modules
 
@@ -19,7 +19,7 @@ date
 echo "Running on $HOSTNAME SPRE analysis"
 
 # Run physics mug model for true value
-python src/simulate_model.py data/physics_mug/input_physics_mug_true4.json 1
+python src/simulate_model.py data/physics_mug/input_physics_mug_true5.json 1
 
 #echo "Node memory state: `free`"
 date
