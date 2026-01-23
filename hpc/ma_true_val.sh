6a0d867efa300bd342cf27e756e147518166ef7e
Richard Howey <richard.howey@ncl.ac.uk>
2026-01-23 18:34:56 +0000

    multi-agent results

---
+++ hpc/ma_true_val.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --partition=long_free
+#SBATCH --account=comet_spread
+#SBATCH --mem=2GB
+#SBATCH --cpus-per-task=1
+#SBATCH --output=data/multi_agent/results/output_multi_agent_true_val.log
+
+# Load modules
+
+module load Python/3.12.3-GCCcore-13.3.0
+
+# Activate virtual environment
+source .venv/bin/activate
+
+# Set temp dir
+TEMPDIR=/nobackup/proj/comet_spread/
+
+date
+echo "Running on $HOSTNAME SPRE analysis"
+
+# Run physics model for true value
+python src/simulate_model.py data/multi_agent/input_multi_agent_true.json 1
+
+#echo "Node memory state: `free`"
+date
+
+echo Total seconds: $SECONDS
